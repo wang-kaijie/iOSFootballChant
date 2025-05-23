9b6ebbbafc608723496e8b407bfbd1a50531b3ce
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2025-05-23 09:51:30 +0800

    Initial Commit

---
+++ FootballChants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootballChants
+//
+//  Created by ByteDance on 23/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
